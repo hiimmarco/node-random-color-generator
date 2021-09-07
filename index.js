42dc204f505bee0e3ff3ef39919e136c320709bd
Marco Bogner <marcobogner@gmail.com>
2021-09-07 16:25:18 +0200

    Add new logic with else if

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ const randomColor = require('randomcolor');
 // Point the variable color to the function that creates a random color in hex
 const color = randomColor();
 
-//Print a random color
-//console.log(randomColor({ hue: 'blue' }));
 
+// Logic for user input
 
-//Print a specified color
-
-
-// If the user has entered a color
 if (process.argv[2]) {
-  // If the color exists, print it out
+
   if (randomColor({ hue: process.argv[2] })) {
     console.log(randomColor({ hue: process.argv[2] }));
+  } else {
+    console.log('Color does not exist');
   }
-  // If the emoji does exist
-  else {
-    console.log('Emoji does not exist');
+} else if (process.argv[2] && process.argv[3]) {
+  if (randomColor({ hue: process.argv[2], luminosity: process.argv[3] })) {
+    console.log(randomColor({ hue: process.argv[2], luminosity: process.argv[3] }));
+  } else {
+    console.log('Color does not exist');
   }
+} else if (process.argv[2] === 'ask') {
+  console.log('Please type a color');
+} else {
+  console.log(randomColor());
 }
