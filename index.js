f407187c0f7b7eeea35a9523e734a3956156581a
Marco Bogner <marcobogner@gmail.com>
2021-09-07 15:03:35 +0200

    Add changes in index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const randomColor = require('randomcolor'); // import the script
+// Import the script
+const randomColor = require('randomcolor');
+
+// Point the variable color to the function that creates a random color in hex
 const color = randomColor();
 
+//Print a random color
 console.log(randomColor());
