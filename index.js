f02326a109aee6a26a019ce4eecb25076a394bf0
Marco Bogner <marcobogner@gmail.com>
2021-09-07 17:31:18 +0200

    Add different loop

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const color = randomColor();
 
 // Logic for user input
 
+const possibleColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome'];
+const possibleLuminosities = ['light', 'dark', 'bright'];
+/*
 if (process.argv[2] && process.argv[3]) {
 
   if (randomColor({ hue: process.argv[2], luminosity: process.argv[3] })) {
@@ -22,6 +25,27 @@ if (process.argv[2] && process.argv[3]) {
   } else {
     console.log('Color does not exist');
   }
+} else {
+  console.log(randomColor());
+}
+*/
+
+let newColor;
+
+if (process.argv[2] && process.argv[3]) {
+  newColor = randomColor({
+    hue: process.argv[2],
+    luminosity: process.argv[3])
+}
+}  else if (process.argv[2] === 'ask') {
+  console.log('Please type a color');
+} else if (process.argv[2]) {
+  newColor = randomColor({
+    hue: process.argv[2]
+  })
+} else {
+  console.log('Color does not exist');
+}
 } else {
   console.log(randomColor());
 }
