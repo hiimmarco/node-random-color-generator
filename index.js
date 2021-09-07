ef3f3a7643d8bcf81a1637787f4960063cbf4360
Marco Bogner <marcobogner@gmail.com>
2021-09-07 15:21:01 +0200

    Add color input

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ const randomColor = require('randomcolor');
 const color = randomColor();
 
 //Print a random color
-console.log(randomColor());
+//console.log(randomColor({ hue: 'blue' }));
+
+
+//Print a specified color
+
+
+// If the user has entered a color
+if (process.argv[2]) {
+  // If the color exists, print it out
+  if (randomColor({ hue: process.argv[2] })) {
+    console.log(randomColor({ hue: process.argv[2] }));
+  }
+  // If the emoji does exist
+  else {
+    console.log('Emoji does not exist');
+  }
+}
