07590c9ca9b83aab916153f82af42ac10abb9b5f
Marco Bogner <marcobogner@gmail.com>
2021-09-10 14:25:18 +0200

    Add console log to each cond

--- index.js
+++ index.js
@@ -8,31 +8,15 @@ const color = randomColor();
 
 let finC;
 
+// If user ionputs hue and luminosity
+
 if (process.argv[2] && process.argv[3]) {
   if (randomColor({ hue: process.argv[2], luminosity: process.argv[3] })) {
     finC = randomColor({
       hue: process.argv[2],
       luminosity: process.argv[3],
     });
-    console.log(
-      randomColor({ hue: process.argv[2], luminosity: process.argv[3] }),
-    );
-  } else {
-    console.log('Color does not exist');
-  }
-} else if (process.argv[2] === 'ask') {
-  console.log('Please type a color');
-} else if (process.argv[2]) {
-  if (randomColor({ hue: process.argv[2] })) {
-    console.log(randomColor({ hue: process.argv[2] }));
-  } else {
-    console.log('Color does not exist');
-  }
-} else {
-  console.log(randomColor());
-}
-
-console.log(`###############################
+    console.log(`###############################
 ###############################
 ###############################
 #####                     #####
@@ -42,3 +26,45 @@ console.log(`###############################
 ###############################
 ###############################
 `);
+  } else {
+    console.log('Color does not exist');
+  }
+
+  // If user types in 'ask'
+} else if (process.argv[2] === 'ask') {
+  console.log('Please type a color');
+}
+
+// If user types in hue
+else if (process.argv[2]) {
+  if (randomColor({ hue: process.argv[2] })) {
+    finC = randomColor({ hue: process.argv[2] });
+    console.log(`###############################
+###############################
+###############################
+#####                     #####
+#####      ${finC}        #####
+#####                     #####
+###############################
+###############################
+###############################
+`);
+  } else {
+    console.log('Color does not exist');
+  }
+}
+
+// If user types in nothing
+else {
+  finC = randomColor();
+  console.log(`###############################
+###############################
+###############################
+#####                     #####
+#####      ${finC}        #####
+#####                     #####
+###############################
+###############################
+###############################
+`);
+}
